04470b1169cf839c955a4e5869302448e5c8661e
Art San <artsan.mcc@gmail.com>
2025-05-27 23:47:59 +0700

    slip

--- src/features/header/home-header.tsx
+++ src/features/header/home-header.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/shared/ui/kit/button'
+
 export function HomeHeader() {
-  return <div>HomeHeader</div>
+  return (
+    <div className="">
+      <h1 className=" text-2xl font-bold underline">HomeHeader</h1>
+      <Button variant={'destructive'}>Жми</Button>
+    </div>
+  )
 }
