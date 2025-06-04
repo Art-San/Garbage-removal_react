827055853cb5a31e637e4332a3d93a35626247f9
Art San <artsan.mcc@gmail.com>
2025-06-04 16:19:56 +0700

    ok

--- src/features/cards-service/card-service.tsx
+++ src/features/cards-service/card-service.tsx
@@ -22,10 +22,11 @@ export const ServiceCard = ({ card }: { card: TProductCardProps }) => {
   return (
     <Card
       key={card.id}
-      className="w-full max-w-[300px] shadow-md transition-transform hover:scale-105"
+      // className="w-full max-w-[300px] shadow-md transition-transform hover:scale-105"
+      className="bg-white rounded-xl shadow-md p-6 transition-all duration-300 hover:shadow-lg hover:-translate-y-1 border border-gray-100 justify-between"
     >
       {/* Изображение */}
-      <div className="relative h-48 overflow-hidden rounded-t-lg">
+      <div className="p-3 bg-blue-50 inline-flex rounded-full mb-4">
         <img src={card.photo_path} alt={card.title} className="object-cover" />
       </div>
 
@@ -36,14 +37,60 @@ export const ServiceCard = ({ card }: { card: TProductCardProps }) => {
       </CardHeader>
 
       {/* Дополнительное содержимое (например, кнопка "Купить") */}
-      <CardContent>
+      <CardContent className="flex justify-end">
         <Button className="cursor-pointer px-6 py-3 bg-[#3644b9] text-white rounded-full font-medium text-center hover:bg-[#2d399c] transition-colors flex items-center justify-center">
           Заказать
         </Button>
-        {/* <button className="w-full bg-primary text-primary-foreground py-2 rounded-md hover:bg-primary/90 transition-colors">
-          Заказать
-        </button> */}
       </CardContent>
     </Card>
   )
 }
+// import { Button } from '@/shared/ui/kit/button'
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle
+// } from '@/shared/ui/kit/card'
+
+// export type TProductCardProps = {
+//   id: string
+//   title: string
+//   description: string
+//   slug: string
+//   price: string
+//   photo_path: string
+//   alt: string
+// }
+
+// export const ServiceCard = ({ card }: { card: TProductCardProps }) => {
+//   console.log(card.photo_path)
+//   return (
+//     <Card
+//       key={card.id}
+//       className="w-full max-w-[300px] shadow-md transition-transform hover:scale-105"
+//     >
+//       {/* Изображение */}
+//       <div className="relative h-48 overflow-hidden rounded-t-lg">
+//         <img src={card.photo_path} alt={card.title} className="object-cover" />
+//       </div>
+
+//       {/* Заголовок и описание */}
+//       <CardHeader>
+//         <CardTitle>{card.title}</CardTitle>
+//         <CardDescription>{card.description}</CardDescription>
+//       </CardHeader>
+
+//       {/* Дополнительное содержимое (например, кнопка "Купить") */}
+//       <CardContent>
+//         <Button className="cursor-pointer px-6 py-3 bg-[#3644b9] text-white rounded-full font-medium text-center hover:bg-[#2d399c] transition-colors flex items-center justify-center">
+//           Заказать
+//         </Button>
+//         {/* <button className="w-full bg-primary text-primary-foreground py-2 rounded-md hover:bg-primary/90 transition-colors">
+//           Заказать
+//         </button> */}
+//       </CardContent>
+//     </Card>
+//   )
+// }
--- src/features/cards-service/cards-service-list.tsx
+++ src/features/cards-service/cards-service-list.tsx
@@ -4,7 +4,8 @@ import { ServiceCard } from './card-service'
 export const CardServiceList = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* <div className="grid grid-cols-3 gap-4"> */}
         {cards_data.map((card) => {
           return <ServiceCard card={card} />
         })}
